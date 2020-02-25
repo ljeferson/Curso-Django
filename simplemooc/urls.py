c5c7decc3b24f39f8b93513781773e359e38e53c
ljeferson <lucasjeferson6000@gmail.com>
2020-02-25 16:10:17 -0300

    Aula 05

---
+++ simplemooc/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'simplemooc.core.views.home', name='home'),
+    url(r'^admin/', include(admin.site.urls)),
+]
